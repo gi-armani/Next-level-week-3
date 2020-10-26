63381c0cf75c3c9a633a253bed702a6b887a231f
gi-armani <giovana_armani@usp.br>
2020-10-26 18:28:46 -0300

    Adicionei mapa na pagina de mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {FiPlus} from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../Images/Map-marker.svg';
 
@@ -21,6 +24,14 @@ function OrphanagesMap() {
                     <span>São Paulo</span>
                 </footer>
             </aside>
+
+            <Map 
+                center = {[-23.6126208,-46.6485248]}
+                zoom = {15}
+                style = {{ width: '100%', height: '100%'}}
+            >
+                <TileLayer url = "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
