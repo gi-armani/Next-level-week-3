45d363eb6b07631647ccfc6ee70ced11d7cb9a64
gi-armani <giovana_armani@usp.br>
2020-10-26 22:43:22 -0300

    Configuracao do banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/users', (request, response) => {
     return response.json({ message: 'Hello World'});
