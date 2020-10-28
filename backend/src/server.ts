904f1e0933e41fd882802f802a92a69697d002ab
gi-armani <giovana_armani@usp.br>
2020-10-28 01:16:25 -0300

    Criacao de rota para insercao de orfanato no banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 import './database/connection';
 
@@ -6,8 +8,28 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-    return response.json({ message: 'Hello World'});
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
 });
 
 app.listen(3333);
