77afbed7d752de5ee1a7038cbb1e3af63bc03dbe
gi-armani <giovana_armani@usp.br>
2020-10-28 23:24:22 -0300

    Rota para busca de orfanato por id

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -11,6 +11,16 @@ export default {
         return response.json(orphanages);
     },
 
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.findOneOrFail();
+
+        return response.json(orphanages);
+    },
+
     async create(request: Request, response: Response) {
         const {
             name,
