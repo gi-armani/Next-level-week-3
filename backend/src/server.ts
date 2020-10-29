05cb9d9cef6bda518366fd13ffe3abf36d4f84fb
gi-armani <giovana_armani@usp.br>
2020-10-28 21:06:55 -0300

    Separacao de rotas em caminho proprio

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,35 +1,12 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.post('/orphanages', async (request, response) => {
-    const {
-        name,
-        latitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    } = request.body;
-
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    });
-
-    await orphanagesRepository.save(orphanage);
-});
 
 app.listen(3333);
