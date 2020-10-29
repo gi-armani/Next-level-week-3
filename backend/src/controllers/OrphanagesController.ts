ea882080c2b458fa0f8ec590adb62d111396c131
gi-armani <giovana_armani@usp.br>
2020-10-28 21:27:36 -0300

    Abstracao com controller baseada em arquitetura mvc

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+export default {
+    async create(request: Request, response: Response) {
+        const {
+            name,
+            latitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+        } = request.body;
+    
+        const orphanagesRepository = getRepository(Orphanage);
+    
+        const orphanage = orphanagesRepository.create({
+            name,
+            latitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+        });
+    
+        await orphanagesRepository.save(orphanage);
+    
+        return response.status(201).json(orphanage);
+    }
+};
